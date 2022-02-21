651b5fae6da6a2a869723ab6091e019332b1d703
muskan848 <68140965+muskan848@users.noreply.github.com>
2022-02-21 12:25:11 +0530

    Add files via upload

---
+++ iNotebook/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import NoteState from './context/notes/NoteState';
+import Alert from "./components/Alert";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+
+
+
+const App = () => {
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+
+
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert alert={alert} />
+
+          <Switch>
+            <Route exact path="/">
+              <Home showAlert={showAlert} />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/login">
+              <Login showAlert={showAlert} />
+            </Route>
+            <Route exact path="/signup">
+              <Signup showAlert={showAlert} />
+            </Route>
+
+          </Switch>
+
+
+        </Router>
+
+      </NoteState>
+    </>
+  )
+}
+export default App;
